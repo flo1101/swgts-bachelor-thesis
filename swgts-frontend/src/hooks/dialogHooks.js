0044aab4f89b331098dc7ce8757f2411bac77beb
Florian Serger <floserger@t-online.de>
2025-01-18 14:13:03 +0100

    FE: add useShallow to useHandleDialog-hook do prevent infinite rerender

--- swgts-frontend/src/hooks/dialogHooks.js
+++ swgts-frontend/src/hooks/dialogHooks.js
@@ -1,27 +1,30 @@
-import {useStore} from "zustand";
+import useStore from "../store";
+import { useShallow } from "zustand/react/shallow";
 
 export const useHandleDialog = () => {
-    const {dialogText, setDialogText, showDialog, setShowDialog} = useStore((state) => ({
+  const { dialogText, setDialogText, showDialog, setShowDialog } = useStore(
+    useShallow((state) => ({
       dialogText: state.dialogText,
       setDialogText: state.setDialogText,
       showDialog: state.showDialog,
       setShowDialog: state.setShowDialog,
-    }));
+    })),
+  );
 
-    const displayDialog = (text) => {
-      setDialogText(text);
-        setShowDialog(true);
-    };
+  const displayDialog = (text) => {
+    setDialogText(text);
+    setShowDialog(true);
+  };
 
-    const closeDialog = () => {
-      setShowDialog(false);
-      setDialogText("")
-    };
+  const closeDialog = () => {
+    setShowDialog(false);
+    setDialogText("");
+  };
 
-    return {
-        displayDialog,
-        closeDialog,
-        showDialog,
-        dialogText
-    };
-}
+  return {
+    displayDialog,
+    closeDialog,
+    showDialog,
+    dialogText,
+  };
+};
