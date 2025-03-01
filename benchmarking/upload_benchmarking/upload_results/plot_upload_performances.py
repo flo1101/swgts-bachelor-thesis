f1518bd9b9fc8101901eac11439a043af12019fa
Florian Serger <floserger@t-online.de>
2025-03-01 18:49:26 +0100

    add script for plotting results of http/socket multi-client upload tests

--- benchmarking/upload_benchmarking/upload_results/plot_upload_performances.py
+++ benchmarking/upload_benchmarking/upload_results/plot_upload_performances.py
@@ -9,7 +9,7 @@ socket_file_path = "socket_upload_performance.csv"
 df_http = pd.read_csv(http_file_path, sep=",")
 df_socket = pd.read_csv(socket_file_path, sep=",")
 
-file_sizes = df_http["File Size (MB)"]
+file_sizes = df_http["File size (MB)"]
 n = len(file_sizes)
 
 bar_width = 0.35
@@ -18,13 +18,13 @@ x = np.arange(n)
 # Plot the data
 plt.figure(figsize=(10, 6))
 
-plt.bar(x - bar_width / 2, df_http["Average Upload Time (s)"], width=bar_width, label="HTTP")
-plt.bar(x + bar_width / 2, df_socket["Average Upload Time (s)"], width=bar_width, label="Socket")
+plt.bar(x - bar_width / 2, df_http["Average upload time (s)"], width=bar_width, label="HTTP")
+plt.bar(x + bar_width / 2, df_socket["Average upload time (s)"], width=bar_width, label="Socket")
 
 plt.xticks(x, file_sizes)
-plt.xlabel("File Size (MB)")
-plt.ylabel("Average Upload Time (s)")
-plt.title("Comparison of HTTP and Socket Upload Performance")
+plt.xlabel("File size (MB)")
+plt.ylabel("Average upload time (s)")
+plt.title("Comparison of HTTP and socket upload performance for different files sizes")
 plt.legend()
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 
