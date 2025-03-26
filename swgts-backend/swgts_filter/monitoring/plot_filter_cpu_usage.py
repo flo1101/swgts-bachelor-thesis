4a2c68649eabf3c4cbe85979b99ddb9b0cdfdae7
Florian Serger <floserger@t-online.de>
2025-03-26 17:44:04 +0100

    adjust plot_filter_cpu_usage.py to work for multiple test runs and specific client counts

--- swgts-backend/swgts_filter/monitoring/plot_filter_cpu_usage.py
+++ swgts-backend/swgts_filter/monitoring/plot_filter_cpu_usage.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-MAX_CLIENT_COUNT = 8
+CLIENT_COUNTS = [1, 4, 8]
+CLIENT_MONITORING_DATA = "../../../benchmarking/upload_benchmarking/monitoring/"
+RUNS = 3
 
 
-def plot_http_socket_cpu_filter_cpu_usage(client_count):
+def plot_filter_cpu_usage(client_count, run, rsf):
     # Get start timestamps
     socket_df = pd.read_csv(
-        f"../../../benchmarking/upload_benchmarking/monitoring/socket/upload_test_monitoring_socket_{client_count}.csv",
+        f"{CLIENT_MONITORING_DATA}socket/test_run_{run}/rsf_{rsf}/upload_test_monitoring_socket_{client_count}.csv",
         skipfooter=1, engine="python")
     http_df = pd.read_csv(
-        f"../../../benchmarking/upload_benchmarking/monitoring/http/upload_test_monitoring_http_{client_count}.csv",
+        f"{CLIENT_MONITORING_DATA}http/test_run_{run}/rsf_{rsf}/upload_test_monitoring_http_{client_count}.csv",
         skipfooter=1, engine="python")
     socket_start_time = socket_df["Timestamp"].iloc[0]
     socket_end_time = socket_df["Timestamp"].iloc[-1]
@@ -18,7 +20,7 @@ def plot_http_socket_cpu_filter_cpu_usage(client_count):
     http_end_time = http_df["Timestamp"].iloc[-1]
 
     # Get cpu usage of filter container
-    cpu_usage_df = pd.read_csv("./filter_cpu_usage.csv")
+    cpu_usage_df = pd.read_csv(f"./test_run_{run}/rsf_{rsf}/filter_cpu_usage.csv")
 
     # Get measurements taken between start and end time
     socket_cpu_usage_df = cpu_usage_df[
@@ -34,20 +36,23 @@ def plot_http_socket_cpu_filter_cpu_usage(client_count):
 
     # Plot data
     plt.figure(figsize=(20, 5))
-    plt.plot(socket_time_diffs, socket_cpu_usage_df["CPU_Percentage"], label="Socket", linestyle="-")
-    plt.plot(http_time_diffs, http_cpu_usage_df["CPU_Percentage"], label="HTTP", linestyle="-")
+    plt.plot(socket_time_diffs, socket_cpu_usage_df["user"], label="Socket", linestyle="-")
+    plt.plot(http_time_diffs, http_cpu_usage_df["user"], label="HTTP", linestyle="-")
 
-    # Add lables and legend
+    # Add labels and legend
     plt.xlabel("Time (s)")
     plt.ylabel("CPU usage (%)")
-    plt.title(f"Filter CPU usage during upload; Clients: {client_count}; RSF: 1")
+    plt.title(f"Filter CPU usage during upload; Run={run}; Clients={client_count}; RSF={rsf}")
     plt.legend()
     plt.grid(True)
 
     # Save and show plot
-    plt.savefig(f"filter_cpu_usage_{client_count}.png")
+    plt.savefig(f"plots/filter_cpu_usage__run_{run}__clients_{client_count}__rsf_{rsf}.png")
     plt.show()
 
 
-for client_count in range(1, MAX_CLIENT_COUNT + 1):
-    plot_http_socket_cpu_filter_cpu_usage(client_count)
+# Plot data for different test runs, client counts and rsf
+for run in range(1, RUNS + 1):
+    for client_count in CLIENT_COUNTS:
+        plot_filter_cpu_usage(client_count, run, 1)
+        plot_filter_cpu_usage(client_count, run, 8)
