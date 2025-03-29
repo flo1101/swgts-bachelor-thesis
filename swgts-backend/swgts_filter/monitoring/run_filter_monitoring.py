29de0006ad9707ac3dd7a72bd8446bf8b7b0727c
Florian Serger <floserger@t-online.de>
2025-03-29 20:21:43 +0100

    only monitor filter container CPU usage instead of the whole application

--- swgts-backend/swgts_filter/monitoring/run_filter_monitoring.py
+++ swgts-backend/swgts_filter/monitoring/run_filter_monitoring.py
@@ -4,16 +4,33 @@ import signal
 import sys
 import time
 
-import psutil
-
 OUTPUT_FILE = "/monitoring/filter_cpu_usage.csv"
+# cgroup files that track CPU usage
+CGROUP_CPU_USAGE = "/sys/fs/cgroup/cpu/cpuacct.usage"
+CGROUP_CPU_STAT = "/sys/fs/cgroup/cpu/cpu.stat"
 
 
 def get_container_cpu_usage(interval):
+    """Returns the CPU usage of the docker container in percent"""
     try:
-        return psutil.cpu_times_percent(interval=interval, percpu=False)
+        # Get time and CPU usage at interval start
+        with open(CGROUP_CPU_USAGE, "r") as f:
+            total_usage_start = int(f.read().strip())
+        system_time_start = time.time()
+
+        time.sleep(interval)
+
+        # Get time and CPU usage at interval end
+        with open(CGROUP_CPU_USAGE, "r") as f:
+            total_usage_end = int(f.read().strip())
+        system_time_end = time.time()
+
+        # Compute CPU usage percentage
+        diff_usage = total_usage_end - total_usage_start
+        diff_time = (system_time_end - system_time_start) * 1e9  # Convert to nanoseconds
+        return (diff_usage / diff_time) * 100
     except Exception as e:
-        print(f"Error getting CPU usage: {e}")
+        print(f"Error getting containers CPU usage: {e}")
         return None
 
 
@@ -35,7 +52,7 @@ def monitor_docker_cpu(interval=1.0, duration=7200):
         while time.time() < start_time + duration:
             cpu_percent = get_container_cpu_usage(interval)
             if cpu_percent is not None:
-                monitoring_data.append([time.time()] + list(cpu_percent))
+                monitoring_data.append([time.time(), cpu_percent])
         save_monitoring_data(monitoring_data)
     except KeyboardInterrupt:
         save_monitoring_data(monitoring_data)
@@ -47,7 +64,7 @@ def save_monitoring_data(data):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    fieldnames = ['Timestamp'] + list(psutil.cpu_times_percent(percpu=False)._fields)
+    fieldnames = ['Timestamp'] + ['CPU_Usage (%)']
 
     with open(OUTPUT_FILE, 'w', newline='') as file:
         writer = csv.writer(file)
